72d6d1206d8eeb2a622432ea8417c02e1b36c52b
Marsh Macy <mmacy@users.noreply.github.com>
2023-12-28 19:33:32 -0800

    del boilerplate comments

--- cli/cmd/character.go
+++ cli/cmd/character.go
@@ -6,7 +6,6 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// firstCmd represents the first command
 var character = &cobra.Command{
 	Use:   "character {create|delete|list|show}",
 	Short: "Manage player characters.",
@@ -18,14 +17,4 @@ var character = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(character)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// firstCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// firstCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
--- cli/cmd/root.go
+++ cli/cmd/root.go
@@ -5,28 +5,21 @@ import (
 )
 
 var rootCmd = &cobra.Command{
-	Use:   "osr", // Name of your application
+	Use:   "osr",
 	Short: "The command-line interface to osrlib.",
 	Long:  `The 'osr' CLI provides command-line access to osrlib and its rules engine.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() error {
 	return rootCmd.Execute()
 }
 
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
+	// TODO: Define flags and configuration settings.
+	//
+	// Persistent flag example:
 	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.myapp.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
+	//
+	// Local flag example:
 	// rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
